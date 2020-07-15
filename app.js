8eae349eeafd18a3b60c956747f817129b400389
Bader Fahoum <fahoumbader@gmail.com>
2020-07-15 11:13:25 +0300

    adding a replicator API funnction

--- app.js
+++ app.js
@@ -22,36 +22,76 @@ fetch("https://api.spoonacular.com/recipes/random?apiKey=078ee27527f84853abaf1d2
 	console.log(err);
 });
 */
+
 const searchform = document.querySelector("#searchform");
+const randomform = document.querySelector("#randomform");
+const results = document.querySelector("#info");
+var resultsArray ;
 // const randomform = 
 // const ingform =
-
-
-//   ** Recipe Search ** EDAMAM API
-const APP_ID = "6e21c9f4";
-const API_KEY = "ddea9e15893170954aaea4c65bad68f0"
-// change "seachfieldid.value" in the line below to search field button id
-fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${encodeURI(seachfieldid.value)}`, { 
-})
-.then(response => {
-  return response.json()
-})
-.then(response=>{
-  // Search dish
-  console.log(response);
-  // Title
-  //const searchRecipeTitle = document.querySelector("#recipe-title");
-  //searchRecipeTitle.textContent = response.recipes[0].title;
-  // recipe summary
-  //const searchRecipeSummary = document.querySelector("#search-recipe-summary");
-  //searchRecipeSummary.innerHTML = response.recipes[0].summary;
+///---------- NORMAL SEARCH
+  searchform.addEventListener('submit', e => {
+    e.preventDefault();
+    let fieldselector = document.querySelector("#foodquery").value;
   
-})
-.catch(err => {
-	console.log(err);
-});
+    console.log(fieldselector);
+    
+  
+    // apicaller(fieldselector);
+    
+    resultsArray = [...resultsSave.json]
+
+    // reset field
+    document.querySelector("#foodquery").value = "";
+  })
+//----------- Random SEARCH
+ randomform.addEventListener('submit',e =>{
+   e.preventDefault();
+   let fieldselector = document.querySelector("#tags").value;
+   document.querySelector("#tags").value = "";
+
+   console.log(fieldselector)
+ })
 
 
+ function replicateAPI(){
+  fetch('./resultsSave.json')
+  .then(response =>  response.json())
+  .then(response => console.log(response.hits))
+
+}
+replicateAPI();
+
+function apicaller(searchfield){
+  //   ** Recipe Search ** EDAMAM API
+  const APP_ID = "6e21c9f4";
+  const API_KEY = "ddea9e15893170954aaea4c65bad68f0"
+  // change "seachfieldid.value" in the line below to search field button id
+  return fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${encodeURI(searchfield)}`, { 
+  })
+  .then(response => {
+    if(response.ok) return response.json()
+    else throw new Error("Something is up with the response ya kbeer");
+  })
+  .then(response=>{
+    resultsArray = [...response.hits];
+    
+
+    // Search dish
+    // console.log(response);
+  //   // Title
+  //   //const searchRecipeTitle = document.querySelector("#recipe-title");
+  //   //searchRecipeTitle.textContent = response.recipes[0].title;
+  //   // recipe summary
+  //   //const searchRecipeSummary = document.querySelector("#search-recipe-summary");
+  //   //searchRecipeSummary.innerHTML = response.recipes[0].summary;
+    
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  
+  }
 
 // ----------------------------- TABS section JS logic ----------------------------------
 function openMenu(evt, tabname) {
